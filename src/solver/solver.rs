5de5ee1df29ae06ba4cb45200c6e50a5edb8fae1
joshc7 <joshuascox@proton.me>
2025-05-10 04:32:34 +0100

    nodepub

--- src/solver/solver.rs
+++ src/solver/solver.rs
@@ -9,13 +9,13 @@ use super::{
 // for non-columns? It's a waste of space for most of the nodes.
 #[derive(Debug)]
 struct Node {
-    pub left: usize,
-    pub right: usize,
-    pub up: usize,
-    pub down: usize,
-    pub col: usize,
-    pub row_label: usize,
-    pub size: usize,
+    left: usize,
+    right: usize,
+    up: usize,
+    down: usize,
+    col: usize,
+    row_label: usize,
+    size: usize,
 }
 
 /// A state of the generator state machine.
